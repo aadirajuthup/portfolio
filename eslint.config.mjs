3113445c8bd77836a12d5bab68828bf804729400
Aadi Raj Uthup <aadi.rajuthup@gmail.com>
2025-01-25 07:54:01 +0530

    Added more info, projects and icons

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+module.exports = {
+  rules: {
+    'prefer-const': 'off',
+  },
+};
+
 export default eslintConfig;
